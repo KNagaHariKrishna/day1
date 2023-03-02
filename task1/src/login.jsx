e5ddc7fb48ac54df54faf85eb8e6e0e7ed57177d
Hari Krishna <122369350+KNagaHariKrishna@users.noreply.github.com>
2023-03-02 22:16:23 +0530

    Update login.jsx

--- task1/src/login.jsx
+++ task1/src/login.jsx
@@ -5,35 +5,61 @@ import { Link, Outlet } from "react-router-dom"
 // import UserDetails from './UserDetails';
 // import { createContext, useContext } from "react";
 // const UserContext = createContext();
+import { useNavigate } from "react-router-dom";
 
 function Login() {
+  const navigate = useNavigate();
   const [userdata, setUserdata] = useState("")
   const [username, setUsername] = useState('')
   const [password, setPassword] = useState("")
-
-  const handleLogin = (e) => {
+  const [response, setResponse] = useState([]);
+  const handleLogin =  async (e) => {
     // e.preventDefault()
     const formData = new FormData()
     formData.append("username", username)
     formData.append("password", password)
 
-    fetch(`http://localhost:5000/userdetails?username=${username}`)
-      .then(res => res.json())
-      .then(data => {
-        setUserdata(data)
-        // alert("login success")
-        console.log(userdata)
-      })
+    // fetch(`http://localhost:5000/userdetails?username=${username}`)
+    //   .then(res => res.json())
+    //   .then(data => {
+    //     setUserdata(data)
+    //     // alert("login success")
+    //     console.log(userdata)
+    //   })
 
 
-    fetch("http://localhost:5000/login", {
-      method: 'POST',
-      body: new URLSearchParams(formData).toString(),
+    // fetch("http://localhost:5000/login", {
+    //   method: 'POST',
+    //   body: new URLSearchParams(formData).toString(),
+    //   headers: {
+    //     // "Authorization": tok,
+    //     'Content-Type': 'application/x-www-form-urlencoded'
+    //   }
+    // },)
+    const resp = await fetch("http://localhost:5000/login", {
+      method: "POST",
       headers: {
-        // "Authorization": tok,
-        'Content-Type': 'application/x-www-form-urlencoded'
-      }
-    },)
+        "Content-Type": "application/json",
+        Accept: "application/json",
+      },
+      body: JSON.stringify({
+        username,
+        password,
+      }),
+      
+    });
+    const data = await resp.json();
+    console.log(data);
+    if (data.token) {
+        localStorage.setItem("jwt", data.token)
+        localStorage.setItem("user", JSON.stringify(data.user))
+        // window.location.href = '/landing'
+        navigate('/UserDetails');
+    }
+    setResponse(data);
+    console.log(response.message === "Login Successful");
+    console.log(JSON.stringify(response) === "Login Successful");
+    console.log(response.token);
   }
 
   return (
@@ -42,10 +68,12 @@ function Login() {
       <input type="text" name="" id="userName" value={username} onChange={(e) => setUsername(e.target.value)} />
       <label htmlFor="password">password</label>
       <input type="text" name="" id="password" value={password} onChange={(e) => setPassword(e.target.value)} />
-      <Link to="/UserDetails">
         <button onClick={handleLogin}>Login</button>
-      </Link>     
-      <Outlet/>
+        <div id="mess">
+              {response.message ? (
+                <div style={{ color: "red" }}>{response.message}</div>
+              ) : null}
+            </div>
     </>
   )
 }
